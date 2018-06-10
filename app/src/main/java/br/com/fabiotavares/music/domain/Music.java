263b471219f6f65dc92e0dc9f72d176a7347a6a7
Fabio Tavares P Rego <fabiotavarespr@github.com>
2018-06-09 21:02:29 -0300

    Removendo Enun Estilo Musical

--- app/src/main/java/br/com/fabiotavares/music/domain/Music.java
+++ app/src/main/java/br/com/fabiotavares/music/domain/Music.java
@@ -2,20 +2,16 @@ package br.com.fabiotavares.music.domain;
 
 import java.io.Serializable;
 
-import br.com.fabiotavares.music.domain.enums.Estilo;
-
 public class Music implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private String nome;
     private String duracao;
-    private Integer estilo;
 
-    public Music(String nome, String duracao, Estilo estilo) {
+    public Music(String nome, String duracao) {
         this.nome = nome;
         this.duracao = duracao;
-        this.estilo = (estilo == null) ? null : estilo.getCod();
     }
 
 
@@ -33,14 +29,6 @@ public class Music implements Serializable {
 
     public void setDuracao(String duracao) {
         this.duracao = duracao;
-    }
-
-    public Integer getEstilo() {
-        return estilo;
-    }
-
-    public void setEstilo(Integer estilo) {
-        this.estilo = estilo;
     }
 
 }
